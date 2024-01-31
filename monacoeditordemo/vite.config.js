8c5ef3b8f7cacf0a83249feafc3c3de153cf8167
zhangyj <33444819+YuanjianZhang@users.noreply.github.com>
2024-01-31 15:35:18 +0800

    fixed bug

--- monacoeditordemo/vite.config.js
+++ monacoeditordemo/vite.config.js
@@ -4,8 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './home/editor/'
-  // base:process.env.NODE_ENV === 'production' 
-  // ? '/Monaca-Editor-Demo' // prod 
-  // : './', // dev
+  base:process.env.NODE_ENV === 'production' 
+  ? './Monaca-Editor-Demo' // prod 
+  : './', // dev
 })
